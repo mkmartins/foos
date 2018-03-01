69188e841a45274f8986505800863bbb9b4be220
Marcos Martins <marcos.martins2307@gmail.com>
2018-03-01 11:30:23 -0700

    Adds third place steaker

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import champ from '../champ-icon.png'
 import loser from '../loser-icon.png'
 import second from '../second.png'
+import goodTry from '../good-try.png'
 
 class Player extends Component {
 	constructor(props) {
@@ -97,6 +98,9 @@ class Player extends Component {
 					}
 					{ this.state.player.name === "Aaron" &&
 						<img src={second} width="400" height="200" className="d-inline-block align-top" alt="" />
+					}
+					{ this.state.player.name === "Nate" &&
+						<img src={goodTry} width="300" height="300" className="d-inline-block align-top" alt="" />
 					}
 					<h3>{this.state.player.name}</h3>
 					<p className="badge badge-secondary">total {this.state.wins - this.state.losses}</p>
